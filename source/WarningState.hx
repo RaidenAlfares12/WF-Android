ca400ee94a72b4c416b94f76e178f4188dafd6c6
BoyfriendlolNewAccount <101530288+BoyfriendlolNewAccount@users.noreply.github.com>
2022-04-24 10:57:26 -0300

    Update WarningState.hx

--- source/WarningState.hx
+++ source/WarningState.hx
@@ -148,14 +148,6 @@ class WarningState extends MusicBeatState
 
 		
 		
-		#if polymod
-		polymod.Polymod.init({modRoot: "mods", dirs: ['introMod']});
-		#end
-		
-		#if sys
-		if (!sys.FileSystem.exists(Sys.getCwd() + "/assets/replays"))
-			sys.FileSystem.createDirectory(Sys.getCwd() + "/assets/replays");
-		#end
 
 		@:privateAccess
 		{
