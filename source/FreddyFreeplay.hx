fffd953a7dbfb5652110cf5792eb0e6bbfbc7598
BoyfriendlolNewAccount <101530288+BoyfriendlolNewAccount@users.noreply.github.com>
2022-04-24 10:59:25 -0300

    Update FreddyFreeplay.hx

--- source/FreddyFreeplay.hx
+++ source/FreddyFreeplay.hx
@@ -248,15 +248,6 @@ class FreddyFreeplay extends MusicBeatState
 		
 
 		
-		
-		#if polymod
-		polymod.Polymod.init({modRoot: "mods", dirs: ['introMod']});
-		#end
-		
-		#if sys
-		if (!sys.FileSystem.exists(Sys.getCwd() + "/assets/replays"))
-			sys.FileSystem.createDirectory(Sys.getCwd() + "/assets/replays");
-		#end
 
 		@:privateAccess
 		{
