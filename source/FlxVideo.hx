dc5a72a809a023392299a8e2737af3a39808bc4e
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2022-04-25 18:22:06 +0800

    Update FlxVideo.hx

--- source/FlxVideo.hx
+++ source/FlxVideo.hx
@@ -50,7 +50,7 @@ class FlxVideo extends FlxBasic {
 
 	        #elseif android
 
-                VideoView.playVideo(AndroidTools.getFileUrl(SUtil.getPath() + name));
+                VideoView.playVideo(AndroidTools.getFileUrl(name));
                 VideoView.onCompletion = function(){
 		        if (finishCallback != null){
 			        finishCallback();
